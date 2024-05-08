16d0518e6b35095f37f57435738324868a00c2e0
Alexander Lazarov <alexander.lazaroff@gmail.com>
2024-05-08 09:48:59 +0300

    Add wezterm config

---
+++ stow/.wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.font_size = 14.0
+
+config.keys = {
+  -- left arrow + option = move one word left
+  { key = "LeftArrow",  mods = "OPT",  action = wezterm.action { SendString = "\x1bb" } },
+  -- right arrow + option = move one word right
+  { key = "RightArrow", mods = "OPT",  action = wezterm.action { SendString = "\x1bf" } },
+  -- left arrow + backspace = delete one word
+  { key = 'Backspace',  mods = 'CTRL', action = wezterm.action.SendKey { key = 'w', mods = 'CTRL' } },
+}
+
+return config
