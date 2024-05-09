0f469b9a3fe755ef096a04039d34d319773e7d29
Alexander Lazarov <alexander.lazaroff@gmail.com>
2024-05-09 08:52:16 +0300

    Add keymaps to switch to prev/next tab

--- stow/.wezterm.lua
+++ stow/.wezterm.lua
@@ -11,6 +11,10 @@ config.keys = {
   { key = "RightArrow", mods = "OPT",  action = wezterm.action { SendString = "\x1bf" } },
   -- left arrow + backspace = delete one word
   { key = 'Backspace',  mods = 'CTRL', action = wezterm.action.SendKey { key = 'w', mods = 'CTRL' } },
+  -- cmd + left arrow = previous tab
+  { key = "LeftArrow",  mods = "CMD",  action = wezterm.action { ActivateTabRelative = -1 } },
+  -- cmd + right arrow = next tab
+  { key = "RightArrow", mods = "CMD",  action = wezterm.action { ActivateTabRelative = 1 } },
 }
 
 config.hide_tab_bar_if_only_one_tab = true
