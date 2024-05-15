1e61e057f821cbf17186a623b8719f1ed9b2a17c
Alexander Lazarov <alexander.lazaroff@gmail.com>
2024-05-15 13:48:52 +0300

    DRY wezterm config

--- stow/.wezterm.lua
+++ stow/.wezterm.lua
@@ -1,20 +1,21 @@
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.font_size = 14.0
 
 config.keys = {
   -- left arrow + option = move one word left
-  { key = "LeftArrow",  mods = "OPT",  action = wezterm.action { SendString = "\x1bb" } },
+  { key = "LeftArrow",  mods = "OPT",  action = act { SendString = "\x1bb" } },
   -- right arrow + option = move one word right
-  { key = "RightArrow", mods = "OPT",  action = wezterm.action { SendString = "\x1bf" } },
+  { key = "RightArrow", mods = "OPT",  action = act { SendString = "\x1bf" } },
   -- left arrow + backspace = delete one word
-  { key = 'Backspace',  mods = 'CTRL', action = wezterm.action.SendKey { key = 'w', mods = 'CTRL' } },
+  { key = 'Backspace',  mods = 'CTRL', action = act.SendKey { key = 'w', mods = 'CTRL' } },
   -- cmd + left arrow = previous tab
-  { key = "LeftArrow",  mods = "CMD",  action = wezterm.action { ActivateTabRelative = -1 } },
+  { key = "LeftArrow",  mods = "CMD",  action = act { ActivateTabRelative = -1 } },
   -- cmd + right arrow = next tab
-  { key = "RightArrow", mods = "CMD",  action = wezterm.action { ActivateTabRelative = 1 } },
+  { key = "RightArrow", mods = "CMD",  action = act { ActivateTabRelative = 1 } },
 }
 
 config.hide_tab_bar_if_only_one_tab = true
