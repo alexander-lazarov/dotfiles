d5706fe13f3c87fc53a25bf753e30f8ced2ef576
Alexander Lazarov <alexander.lazaroff@gmail.com>
2024-07-08 10:34:28 +0300

    Automatically adjust wezterm colours based on system theme

--- stow/.wezterm.lua
+++ stow/.wezterm.lua
@@ -34,5 +34,23 @@ config.keys = {
 
 config.hide_tab_bar_if_only_one_tab = true
 
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Builtin Solarized Dark"
+	else
+		return "Builtin Solarized Light"
+	end
+end
+
+wezterm.on("window-config-reloaded", function(window)
+	local overrides = window:get_config_overrides() or {}
+	local appearance = window:get_appearance()
+	local scheme = scheme_for_appearance(appearance)
+	if overrides.color_scheme ~= scheme then
+		overrides.color_scheme = scheme
+		window:set_config_overrides(overrides)
+	end
+end)
+
 return config
 -- vim: ts=2 sts=2 sw=2 et
